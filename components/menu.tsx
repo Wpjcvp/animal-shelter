4d454ec2e6314a15762779bb67fa12ecf415393c
Wpjcvp <ssuperfriend@mail.ru>
2023-05-17 00:30:05 +0300

    next

---
+++ components/menu.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const MenuItem = ({ title, link }: { title: string; link: string }) => {
+  return (
+    <Link className="px-5 py-5" href={link}>
+      {title}
+    </Link>
+  );
+};
+
+export const Menu = () => {
+  return (
+    <div className="h-[110px] w-full bg-[#1a2026] flex items-end justify-center space-x-10 uppercase font-semibold">
+      <MenuItem title="Главная" link="/" />
+      <MenuItem title="о нас" link="/about" />
+      <MenuItem title="щенки" link="/puppies" />
+      <MenuItem title="собаки" link="/dogs" />
+      <MenuItem title="нашли дом" link="/home" />
+      <MenuItem title="поддержка" link="/support" />
+      <MenuItem title="контакты" link="/contact" />
+    </div>
+  );
+};
